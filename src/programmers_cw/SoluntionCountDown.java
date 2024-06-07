a51e711eb751c51f72d61db8835c777117091739
chanwoohi <chanwoohi@naver.com>
2024-06-07 17:50:51 +0900

    카운트 다운 배열

--- src/programmers_cw/SoluntionCountDown.java
+++ src/programmers_cw/SoluntionCountDown.java
@@ -1,10 +1,16 @@
 package programmers_cw;
 
+import java.util.Arrays;
+
 public class SoluntionCountDown {
 
+	public int[] solution(int start_num, int end_num) {
+		int[] answer = {};
+		for( int i = start_num ; i >= end_num ; i-- ) {
+			answer[i] = i - 1;
+		}
+		return answer;
+    	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
 	}
-
 }
