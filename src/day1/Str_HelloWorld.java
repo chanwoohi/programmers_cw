fc6ffc7f97ee0fd55f5f59e2e5d201e78a3af1b7
chanwoohi <chanwoohi@naver.com>
2024-06-07 17:50:25 +0900

    기초테스트 출력

---
+++ src/day1/Str_HelloWorld.java
@@ -0,0 +1,13 @@
+package day1;
+
+import java.util.Scanner;
+
+public class Str_HelloWorld {
+	    public static void main(String[] args) {
+	        Scanner sc = new Scanner(System.in);
+	        System.out.print("");
+	        String a = sc.next();
+	        System.out.println("" + a);
+	    }
+	
+}
