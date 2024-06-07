4308944dd1c6ad9c7ba46c6c6324c43e908cc37c
chanwoohi <chanwoohi@naver.com>
2024-06-07 17:50:15 +0900

    기초테스트 반복 출력

---
+++ src/day1/RefeatOutput.java
@@ -0,0 +1,18 @@
+package day1;
+
+import java.util.Scanner;
+
+public class RefeatOutput {
+	
+	    public static void main(String[] args) {
+	        Scanner sc = new Scanner(System.in);
+	        String str = sc.next();
+	        int n = sc.nextInt();
+	        
+	        for(int i = 1 ; i <= n ; i++) {
+	            System.out.print(str);
+	        }
+	    }
+	
+
+}
