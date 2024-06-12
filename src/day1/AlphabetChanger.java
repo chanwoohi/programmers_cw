49d7e0766ac0fe13089a9b20ccb42ad553caadf9
chanwoohi <chanwoohi@naver.com>
2024-06-12 17:57:22 +0900

    알파벳 대소문자 바꾸기

--- src/day1/AlphabetChanger.java
+++ src/day1/AlphabetChanger.java
@@ -7,19 +7,18 @@ public class AlphabetChanger {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
         String a = sc.next();
-        char b;
-        String c = "";
         
-        for(int i=0; i< a.length(); i++){
-            b = a.charAt(i);
-            if(Character.isUpperCase(b)){
-                c += Character.toLowerCase(b);
-             
-            } else {
-                c += Character.toUpperCase(b);
+        String answer = "";
+        
+        for(int i = 0; i<a.length(); i++){
+            char c = a.charAt(i);
+            if(Character.isUpperCase(c)){
+                answer += Character.toLowerCase(c);
+            }else{
+                answer += Character.toUpperCase(c);
             }
         }
-        System.out.println(c);
+       System.out.println(answer);
 	}
 
 }
