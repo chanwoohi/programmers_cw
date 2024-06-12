26ef78137ac1edb2591a26364da282c58426361b
chanwoohi <chanwoohi@naver.com>
2024-06-12 17:57:29 +0900

    덮어쓰기?

---
+++ src/day2/Overlay.java
@@ -0,0 +1,20 @@
+package day2;
+
+public class Overlay {
+
+	public String solution(String my_string, String overwrite_string, int s) {
+		String answer = "";
+		answer += my_string.substring(0, s);
+		answer += overwrite_string;
+		if(my_string.length() - overwrite_string.length() > 0) {
+			answer += my_string.substring(s + overwrite_string.length());
+		}
+		
+		
+		
+		
+		return answer;
+
+	}
+
+}
