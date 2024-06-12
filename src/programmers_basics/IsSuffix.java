a90b4125a0e5805be4dc0865d5ea4edaca717f56
chanwoohi <chanwoohi@naver.com>
2024-06-12 18:43:01 +0900

    끝나는 말

---
+++ src/programmers_basics/IsSuffix.java
@@ -0,0 +1,9 @@
+package programmers_basics;
+
+public class IsSuffix {
+	public int solution(String my_string, String is_suffix) {
+		int answer = 0;
+		if(my_string.endsWith(is_suffix)) return 1;
+		return answer;
+	}
+}
