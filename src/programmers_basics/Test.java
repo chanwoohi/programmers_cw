032296f246c74b072303ff4b1e71f1887bee61e3
chanwoohi <chanwoohi@naver.com>
2024-06-12 20:28:10 +0900

    프로그래머스 실험실

--- src/programmers_basics/Test.java
+++ src/programmers_basics/Test.java
@@ -5,20 +5,14 @@ import java.util.Arrays;
 public class Test {
 
 	public static void main(String[] args) {
-		String A = "League Of Legend";
-		String a = "L";
-		String b = "Le";
-		String c = "La";
-		String d = "Lea";
-		String e = "L eagu";
-		String f = "League Of L";
-
-		System.out.println(A.startsWith(a));
-		System.out.println(A.startsWith(b));
-		System.out.println(A.startsWith(c));
-		System.out.println(A.startsWith(d));
-		System.out.println(A.startsWith(e));
-		System.out.println(A.startsWith(f));
+		int[] numbers = {1, 2, 100, -99, 1, 2, 3 };
+		int[] answer = new int [numbers.length];
+		
+        for(int i = 0 ; i < numbers.length ; i++) {
+        	answer[i] = numbers[i]*2;
+        }
+		System.out.println(Arrays.toString(answer));
 	}
+	
 
 }
