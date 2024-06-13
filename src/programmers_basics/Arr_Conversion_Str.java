5c3b97b8277cee60f56739893d16a4847aab84be
chanwoohi <chanwoohi@naver.com>
2024-06-13 19:52:18 +0900

    배열을 문자열으로

---
+++ src/programmers_basics/Arr_Conversion_Str.java
@@ -0,0 +1,11 @@
+package programmers_basics;
+
+public class Arr_Conversion_Str {
+	public String solution(String[] arr) {
+        String answer = "";
+        for(int i = 0 ; i < arr.length ; i++) {
+        	answer += arr[i];
+        }
+        return answer;
+    }
+}
