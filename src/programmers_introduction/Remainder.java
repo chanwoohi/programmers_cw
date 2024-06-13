1e10c9b3ae4c131caa3535ec30f3f977f0eb604c
chanwoohi <chanwoohi@naver.com>
2024-06-13 16:48:06 +0900

    모드

---
+++ src/programmers_introduction/Remainder.java
@@ -0,0 +1,8 @@
+package programmers_introduction;
+	// 나누기 나머지
+public class Remainder {
+	public int solution(int num1, int num2) {
+        int answer = num1 % num2;
+        return answer;
+    }
+}
