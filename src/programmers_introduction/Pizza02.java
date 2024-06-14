4b942ed0ba8fe55eabe47543cb549e09ca534236
KimHyeonjung <kardel@naver.com>
2024-06-14 19:34:42 +0900

    피자 2

--- src/programmers_introduction/Pizza02.java
+++ src/programmers_introduction/Pizza02.java
@@ -3,7 +3,15 @@ package programmers_introduction;
 public class Pizza02 {
 	public int solution(int n) {
         int answer = 0;
+        int a = 0;
+        for( int i = 1 ; i <= n && i <= 6 ; i++) {
+        	if(n % i == 0 && 6 % i == 0) {
+        		a = i;
+        	}
+        }
+        int b = (n * 6)/a;
         
+        answer = b / 6;
         return answer;
     }
 }
