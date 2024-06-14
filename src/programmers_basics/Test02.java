6eb700cbcfd389525abbb6831171d608811712e3
KimHyeonjung <kardel@naver.com>
2024-06-14 19:34:34 +0900

    Update Test02.java

--- src/programmers_basics/Test02.java
+++ src/programmers_basics/Test02.java
@@ -5,44 +5,7 @@ import java.util.Arrays;
 public class Test02 {
 
 	public static void main(String[] args) {
-		String ineq = "<", eq = "=";
-		int n = 20, m = 50;
-		int answer = 0;
-		if ( ineq == "<" ){
-			if(eq == "=") {
-				if(n <= m) {
-					answer = 1; 
-				}
-				else {
-					answer = 0;
-				}
-			}
-			else {
-				if(n < m) {
-					answer = 1; 
-				}
-				else {
-					answer = 0;
-				}
-			}
-		}
-		else {
-			if(eq == "=") {
-			}
-			if(n >= m) {
-				answer = 1; 
-			}
-			else {
-				answer = 0;
-			}
-			if(n > m) {
-				answer = 1; 
-			}
-			else {
-				answer = 0;
-			}
-		}
-		System.out.println(answer);
+		
 	}
 
 }
