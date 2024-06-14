bb40fb18da2124ebb154ea8d67fda8bdd8ea647c
KimHyeonjung <kardel@naver.com>
2024-06-14 19:34:03 +0900

    조건 연산자

--- src/programmers_basics/ConditionString.java
+++ src/programmers_basics/ConditionString.java
@@ -2,24 +2,25 @@ package programmers_basics;
 
 public class ConditionString {
 	public int solution(String ineq, String eq, int n, int m) {
-		int answer = 0;
-		if ( ineq == "<" ){
-			if(eq == "=") {
-				if(n <= m) return 1; 
-				return 0;
-			}
-			else {
-				if(n < m) return 1; 
-				return 0;
-			}
-		}
-		if(eq == "=") {
-			if(n >= m) return 1; 
-			return 0;
-		}
-		else {
-			if(n > m) return 1; 
-			return 0;
-		}
+		 
+		
+		int answer =0;
+		if (n < m) {
+	            return ineq.equals("<") ? 1 : 0;
+	        } else if (n > m) {
+	        	
+	            return ineq.equals(">") ? 1 : 0;
+	        } else {
+	            return eq.equals("=") ? 1 : 0;
+	        }
+
+	
 	}
+	
+
+
+
 }
+
+
+
