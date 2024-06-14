64d01ce62d9c6a025be1f3857a1390f244a27f92
chanwoohi <chanwoohi@naver.com>
2024-06-14 12:51:40 +0900

    배수찾기

---
+++ src/programmers_basics/SearchMultiple.java
@@ -0,0 +1,11 @@
+package programmers_basics;
+
+public class SearchMultiple {
+	public int solution(int num, int n) {
+        int answer = 0;
+        if(num % n == 0) {
+        	return 1;
+        }
+        return answer;
+    }
+}
