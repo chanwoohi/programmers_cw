fa6b9a1145c363efbdfae77b359350b930fb34ae
KimHyeonjung <kardel@naver.com>
2024-06-14 19:34:28 +0900

    플래그 바이 플래그

---
+++ src/programmers_basics/Flag_by_Flag.java
@@ -0,0 +1,16 @@
+package programmers_basics;
+
+public class Flag_by_Flag {
+	public int solution(int a, int b, boolean flag) {
+   
+        if(flag) {
+        	return a + b;
+        }
+        return a- b;
+        
+        
+        
+ 
+    }
+	
+}
