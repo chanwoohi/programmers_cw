c4a82ca7c7dbbf78371e81012af8ccff1caae32e
KimHyeonjung <kardel@naver.com>
2024-06-17 19:31:36 +0900

    머쓱이네 옷가게

--- src/programmers_basics/Test02.java
+++ src/programmers_basics/Test02.java
@@ -5,7 +5,17 @@ import java.util.Arrays;
 public class Test02 {
 
 	public static void main(String[] args) {
-		
+		int price = 500000;
+        if(price >= 500000) {
+        	System.out.println(  price * 0.8 );
+        }
+        else if (price >= 300000) {
+        	System.out.println( price * (9 / 10));
+        }
+        else if (price >= 100000) {
+        	System.out.println(price * (19 / 20));
+        }
+        System.out.println(price);
 	}
 
 }
