0e2096dd80cdd19c175efd0aa2399ca1523ef8ca
KimHyeonjung <kardel@naver.com>
2024-06-18 17:49:44 +0900

    세균 증식

---
+++ src/programmers_introduction/Bacteria.java
@@ -0,0 +1,10 @@
+package programmers_introduction;
+
+public class Bacteria {
+	public int solution(int n, int t) {
+		for(int i = 1 ; i <= t ; i++) {
+        	n *= 2;
+        }
+        return n;
+    }
+}
