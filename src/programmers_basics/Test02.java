89cd97ab0aab5e87b341e5e25092b5332af02c0b
KimHyeonjung <kardel@naver.com>
2024-06-18 17:49:36 +0900

    Update Test02.java

--- src/programmers_basics/Test02.java
+++ src/programmers_basics/Test02.java
@@ -5,17 +5,10 @@ import java.util.Arrays;
 public class Test02 {
 
 	public static void main(String[] args) {
-		int price = 500000;
-        if(price >= 500000) {
-        	System.out.println(  price * 0.8 );
+		int n = 2;
+		for(int i = 1 ; i <= 10 ; i++) {
+        	n *= 2;
         }
-        else if (price >= 300000) {
-        	System.out.println( price * (9 / 10));
-        }
-        else if (price >= 100000) {
-        	System.out.println(price * (19 / 20));
-        }
-        System.out.println(price);
 	}
 
 }
