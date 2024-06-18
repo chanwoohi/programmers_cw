5f72adf2c4aa4af343a98c97ffac5ebd3ec2cd65
KimHyeonjung <kardel@naver.com>
2024-06-18 17:50:12 +0900

    문자열에서 특정한 문자 지우기

---
+++ src/programmers_introduction/RemoveCertainString.java
@@ -0,0 +1,7 @@
+package programmers_introduction;
+
+public class RemoveCertainString {
+	public String solution(String my_string, String letter) {
+		return  my_string.replace(letter, "");
+    }
+}
