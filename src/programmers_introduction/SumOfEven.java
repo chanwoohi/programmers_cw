2863cad4aa89f8268c04f2d5f83397ed9c2c883e
KimHyeonjung <kardel@naver.com>
2024-06-18 17:50:42 +0900

    짝수 다 더하기

---
+++ src/programmers_introduction/SumOfEven.java
@@ -0,0 +1,13 @@
+package programmers_introduction;
+
+public class SumOfEven {
+	 public int solution(int n) {
+	        int answer = 0;
+	        for(int i = 1 ; i <= n ; i++) {
+	        	if( i % 2 == 0) {
+	        		answer += i;
+	        	}
+	        }
+	        return answer;
+	    }
+}
