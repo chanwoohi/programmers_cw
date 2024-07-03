106fb7939d035775e3fe03e37249376bca4808ac
KimHyeonjung <kardel@naver.com>
2024-07-03 12:46:10 +0900

    암호 해석하기

---
+++ src/programmers_introduction/EncodingCipher.java
@@ -0,0 +1,13 @@
+package programmers_introduction;
+
+public class EncodingCipher {
+
+	public String solution(String cipher, int code) {
+        String answer = "";
+        for(int i = code - 1 ; i <= cipher.length() ; i += code ) {
+        	answer = cipher.substring(i, i+1);
+        }
+        return answer;
+    }
+
+}
