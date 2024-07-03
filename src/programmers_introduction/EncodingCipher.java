b018b91274075d2060a84b185f8a13bb2783010b
KimHyeonjung <kardel@naver.com>
2024-07-03 17:49:17 +0900

    암호 해독하기

--- src/programmers_introduction/EncodingCipher.java
+++ src/programmers_introduction/EncodingCipher.java
@@ -4,8 +4,8 @@ public class EncodingCipher {
 
 	public String solution(String cipher, int code) {
         String answer = "";
-        for(int i = code - 1 ; i <= cipher.length() ; i += code ) {
-        	answer = cipher.substring(i, i+1);
+        for(int i = code - 1 ; i <= cipher.length() -1 ; i += code ) {
+        	answer += cipher.substring(i, i+1);
         }
         return answer;
     }
